cb7b3f1850d8af65d17da7ff75b4e5e0e1ee3cbc
Konstantinos Despoinidis <kdesp2003@gmail.com>
2024-05-08 21:10:05 +0300

    fixed my toys scroll

--- app/src/main/java/io/github/kdesp73/petadoption/room/AppDatabase.kt
+++ app/src/main/java/io/github/kdesp73/petadoption/room/AppDatabase.kt
@@ -3,7 +3,11 @@ package io.github.kdesp73.petadoption.room
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [LocalUser::class, Settings::class, LocalPet::class, LocalToy::class], version = 10, exportSchema = false)
+@Database(
+    entities = [LocalUser::class, Settings::class, LocalPet::class, LocalToy::class],
+    version = 10,
+    exportSchema = false
+)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun userDao(): LocalUserDao
     abstract fun settingsDao(): SettingsDao
--- app/src/main/java/io/github/kdesp73/petadoption/routes/MyToys.kt
+++ app/src/main/java/io/github/kdesp73/petadoption/routes/MyToys.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -109,6 +110,7 @@ fun MyToys(room: AppDatabase, navController: NavController){
                 Column (
                     modifier = Modifier
                         .padding(8.dp)
+                        .verticalScroll(scrollState)
                         .fillMaxSize(),
                     horizontalAlignment = Alignment.CenterHorizontally,
                     verticalArrangement = Arrangement.spacedBy(15.dp)
